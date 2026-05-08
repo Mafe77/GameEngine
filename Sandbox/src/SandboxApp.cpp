ed2acbddd7b981c2de157f499604611b1f83bcb1
Mafe77 <fefepanda@gmail.com>
2026-05-08 15:01:05 -0400

    Key and mouse codes defined

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -10,12 +10,13 @@ public:
 
 	void OnUpdate() override
 	{
-		GE_INFO("ExampleLayer::Update");
+		if (GameEngine::Input::IsKeyPressed(GE_KEY_TAB))
+			GE_INFO("Tab key is pressed");
 	}
 
 	void OnEvent(GameEngine::Event& event) override
 	{
-		GE_TRACE("{0}", event);
+		//GE_TRACE("{0}", event);
 	}
 };
 
