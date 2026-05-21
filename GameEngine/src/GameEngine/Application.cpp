46e9de42c204929b32152f62440d371df5dd3ade
Mafe77 <fefepanda@gmail.com>
2026-05-21 16:00:52 -0400

    blending setup

--- GameEngine/src/GameEngine/Application.cpp
+++ GameEngine/src/GameEngine/Application.cpp
@@ -19,6 +19,8 @@ namespace GameEngine
 		m_Window = Scope<Window>(Window::Create());
 		m_Window->SetEventCallback(GE_BIND_EVENT_FN(Application::OnEvent));
 
+		Renderer::Init();
+
 		m_ImGuiLayer = new ImGuiLayer();
 
 		PushOverlay(m_ImGuiLayer);
--- GameEngine/src/GameEngine/Renderer/Renderer.cpp
+++ GameEngine/src/GameEngine/Renderer/Renderer.cpp
@@ -8,6 +8,11 @@ namespace GameEngine
 {
 	Renderer::SceneData* Renderer::m_SceneData = new Renderer::SceneData;
 
+	void Renderer::Init()
+	{
+		RenderCommand::Init();
+	}
+
 	void Renderer::BeingScene(OrthographicCamera& camera)
 	{
 		m_SceneData->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
